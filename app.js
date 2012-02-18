5adf7c383358655310ab410ee10d0b086e5064f6
Stephan Wallentin <stephan.wallentin@ef.com>
2012-02-19 02:04:52 +0800

    port fix

--- app.js
+++ app.js
@@ -32,5 +32,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
